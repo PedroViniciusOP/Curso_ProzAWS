96f9acd6b15d3c936687f9dd13b487a21fcf37a5
PedroViniciusOP <39882683+PedroViniciusOP@users.noreply.github.com>
2023-07-31 13:32:02 -0300

    Calc_Case.py

--- Calc_Case.py
+++ Calc_Case.py
@@ -1,6 +1,6 @@
-#** Desafio 10 **
+# ** Desafio 10 **
 #
-#Faça uma função calculadora que os números e as operações serão feitas pelo usuário.
+# Faça uma função calculadora que os números e as operações serão feitas pelo usuário.
 # O código deve ficar rodando infinitamente até que o usuário escolha a opção de sair. No início, o programa mostrará a seguinte lista de operações:
 # 1: Soma
 # 2: Subtração
